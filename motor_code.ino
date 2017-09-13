e3f4f342d489925b4742e172c3a2a87699915e2b
dannybellis <dannybellis@gmail.com>
2017-09-13 12:11:58 -0500

    Add files via upload

---
+++ motor_code.ino
@@ -0,0 +1,15 @@
+#include <Adafruit_MotorShield.h>
+Adafruit_MotorShield AFMS = Adafruit_MotorShield(); 
+Adafruit_DCMotor *myMotor = AFMS.getMotor(1);
+
+void setup() {
+  // put your setup code here, to run once:
+  AFMS.begin();
+  myMotor->setSpeed(0);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  myMotor->run(FORWARD);
+  myMotor->setSpeed(255);
+
