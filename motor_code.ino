0ab44e0bc465d230768974d053180c43a90b3b17
dannybellis <dannybellis@gmail.com>
2017-09-13 12:49:32 -0500

    Update motor_code.ino
    Added speed, direction and servo control through serial communication on 9600. Enter commands in this order: speed, direction, servo turn.

--- motor_code.ino
+++ motor_code.ino
@@ -1,15 +1,28 @@
 #include <Adafruit_MotorShield.h>
+#include <Servo.h>
 Adafruit_MotorShield AFMS = Adafruit_MotorShield(); 
 Adafruit_DCMotor *myMotor = AFMS.getMotor(1);
+Servo servo1;
 
 void setup() {
-  // put your setup code here, to run once:
   AFMS.begin();
   myMotor->setSpeed(0);
+  servo1.attach(9);
+  Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  myMotor->run(FORWARD);
-  myMotor->setSpeed(255);
-
+  while (Serial.available() > 4){
+    int spd = Serial.parseInt();
+    int dir = Serial.parseInt();
+    int turn = Serial.parseInt();
+    myMotor->setSpeed(spd);
+    servo1.write(turn);
+    if (dir == 1){
+      myMotor->run(FORWARD);
+      }
+    if (dir == 0){
+      myMotor->run(BACKWARD);
+      } 
+    } 
+  }
