5b48d0b0be60078b24ed5b599388566ca037ecbd
mich-431 <28988176+mich-431@users.noreply.github.com>
2017-10-10 11:30:45 -0500

    Server_2 from pi
    Update Server_2 to drive and with camera stuff

---
+++ server_2.py
@@ -0,0 +1,50 @@
+
+import socket
+import time
+import serial
+import picamera
+import csv
+
+with picamera.PiCamera() as camera:
+    camera.resolution = (150, 150)
+    time.sleep(2)
+moves = open('moves.csv', 'w')
+moves_csv = csv.writer(moves)
+
+
+HOST = ''
+PORT = 1338
+ser = serial.Serial('/dev/ttyACM0', 9600)
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((HOST, PORT))
+s.listen(5)
+print('A server opened at host ' + socket.gethostname() + ' on port ' + str(PORT)  + '!')
+
+conn, addr = s.accept()
+print(addr[0] + ' has connected!')
+x = 0
+data2 = 0
+while True:
+    data = conn.recv(1)
+    if data == data2:
+        with picamera.PiCamera() as camera:
+            camera.capture("/mnt/usb/img/" + str(x) + ".jpg")
+        x += 1
+        moves.write(str(data2) + ', ' )
+        continue
+    if not data: break
+    if data == b'7':
+        ser.write(b'0')
+        ser.close()
+        conn.close()
+        moves.close()
+        print('The server has been closed!')
+        break
+    else:
+        ser.write(data)
+        moves.write(str(data)+', ')
+        with picamera.PiCamera() as camera:
+            camera.capture("/mnt/usb/img/" +str(x) + ".jpg")
+            x += 1
+    data2 = data
+	
