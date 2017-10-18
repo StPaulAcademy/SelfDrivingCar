c20dae252356a4694218c425860d73299c12c281
mich-431 <28988176+mich-431@users.noreply.github.com>
2017-10-18 17:14:37 -0500

    move alexnet to branch
    Organize each type of net in its own branch.

---
+++ train_alexnet.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct  3 12:15:59 2017
+
+@author: 18MichaelRH
+"""
+
+import numpy as np
+from alexnet import alexnet
+
+WIDTH = 150
+HEIGHT = 150
+LR = 1e-3
+EPOCHS = 10
+MODEL_NAME = 'APTnet-{}-{}-{}-epochs.model'.format(LR, 'alexnet', EPOCHS)
+
+model = alexnet(WIDTH, HEIGHT, LR)
+print("model loaded")
+
+train_data = np.load('trainingdata.npy')
+print('loaded data')
+print(np.shape(train_data[2][0]))
+
+train = train_data[:-5]
+test = train_data[-5:]
+
+X = np.array([i[0] for i in train]).reshape(-1, WIDTH, HEIGHT, 1)
+Y = [i[1] for i in train]
+
+#print(np.shape(X), X)
+
+test_x = np.array([i[0] for i in test]).reshape(-1, WIDTH, HEIGHT, 1)
+test_y = [i[1] for i in test]
+
+model.fit({'input': X}, {'targets': Y}, n_epoch=EPOCHS, validation_set=({'input': test_x}, {'targets': test_y}), snapshot_step=500, show_metric = True, run_id=MODEL_NAME)
+
+model.save(MODEL_NAME)
