7f5e74ee4513097298c3c15eae2529d10e2c7e9d
mich-431 <28988176+mich-431@users.noreply.github.com>
2017-10-28 20:46:39 -0500

    Update train_inceptionv3.py
    Update to work

--- train_inceptionv3.py
+++ train_inceptionv3.py
@@ -8,6 +8,9 @@ Created on Tue Oct  3 12:15:59 2017
 import numpy as np
 from inceptionv3 import inceptionv3
 
+TRIAL = 0
+SAMPLES = 648
+
 WIDTH = 150
 HEIGHT = 150
 LR = 1e-3
@@ -16,16 +19,18 @@ MODEL_NAME = 'APTnet-{}-{}-{}-epochs.model'.format(LR, 'inceptionv3', EPOCHS)
 
 model = inceptionv3(WIDTH, HEIGHT, LR)
 
-train_data = np.load('TRAINDATA.npy')
+train_data = np.load('trainingdata-{}-{}.npy'.format(TRIAL, SAMPLES))
 
-train = train_data[:-500]
-test = train_data[-500:]
+train = train_data[:-5]
+test = train_data[-5:]
 
-X = np.array(i[0] for i in train).reshape(-1, WIDTH, HEIGHT, 1)
+X = np.array([i[0] for i in train]).reshape(-1, WIDTH, HEIGHT, 1)
 Y = [i[1] for i in train]
 
-test_x = np.array(i[0] for i in test).reshape(-1, WIDTH, HEIGHT, 1)
+test_x = np.array([i[0] for i in test]).reshape(-1, WIDTH, HEIGHT, 1)
 test_y = [i[1] for i in test]
+
+print(np.shape(X), np.shape(Y), np.shape(test_x), np.shape(test_y))
 
 model.fit({'input': X}, {'targets': Y}, n_epoch=EPOCHS, validation_set=({'input': test_x}, {'targets': test_y}), snapshot_step=500, show_metric = True, run_id=MODEL_NAME)
 
